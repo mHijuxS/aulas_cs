263c95df5feea44e26251b62c2a61a5664b6f61c
mHijuxS <matheus_sf@outlook.com.br>
2023-06-14 17:54:57 -0300

    Adicionando interface

---
+++ ExFixInter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using ExFixInter.Entities;
+using ExFixInter.Services;
+
+namespace ExFixInter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine()!);
+            Console.Write("Data (dd/MM/yyyy): ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine()!, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double value = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installments: ");
+            int n_installments = int.Parse(Console.ReadLine()!);
+
+            Contract cont = new Contract(number, date, value);
+
+            ContractService service = new ContractService(new PaypalServices());
+            service.ProcessContract(cont, n_installments);
+
+
+            Console.WriteLine("Installments:");
+
+            
+            foreach(Installment inst in cont.Installments)
+            {
+                Console.WriteLine(inst);
+            }
+
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model:");
+            Console.Write("Pickup (dd/MM/yyyy) ");
+            Console.Write("Return (dd/MM/yyyy) ");
+            Console.Write("Enter price per hour: ");
+            Console.Write("Enter price per day: ");
+            Console.WriteLine("INVOICE");
+
+        }
+    }
+}
